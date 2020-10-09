6ba5a403f90b0c7e77b6bdfbaba317043e29608c
stepin105197 <69113491+stepin105197@users.noreply.github.com>
2020-10-09 19:36:11 +0530

    Create sai.c

---
+++ src/sai.c
@@ -0,0 +1,40 @@
+
+#include<stdio.h>
+#include<string.h>
+int veena()
+{
+    return 600;
+}
+int violin()
+{
+    return 500;
+}
+int chitraveena()
+{
+    return 550;
+}
+int flute()
+{
+    return 500;
+}
+int saxophone()
+{
+    return 550;
+}
+int nadaswaram()
+{
+    return 300;
+}
+int mrudangam()
+{
+    return 500;
+}
+int tabla()
+{
+    return 500;
+}
+int ghatam()
+{
+    return 500;
+}
+
